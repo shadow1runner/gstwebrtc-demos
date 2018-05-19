498ea45a5e3d887c2c1369ccb88e8291652a7b31
Helmut Wolf <helmut.wolf@outlook.com>
2018-05-19 20:26:20 +0200

    Streaming works on boot up :)

--- raspberry/start-streaming.sh
+++ raspberry/start-streaming.sh
@@ -1,4 +1,7 @@
 #! /bin/sh
-../../../cerbero/cerbero-uninstalled shell
+#../../cerbero/cerbero-uninstalled shell
+
+export LD_LIBRARY_PATH=/home/helli/dev/cerbero/build/dist/linux_armv7/lib:$LD_LIBRARY_PATH
+
 ./webrtc-sendrecv --server wss://hexy.westeurope.cloudapp.azure.com:8443/ --peer-id 666 $1 $2 $3 $4
 
